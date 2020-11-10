5e725559ab1fd447043f631c240421cf560790c3
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-11-11 00:17:52 +0300

    Code style checker

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-6">
-        <a class="navbar-brand" href="#">textie</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-6">
+        <a className="navbar-brand" href="#">textie</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link" href="#">Courses</a>
-            <a class="nav-link" href="#">Map</a>
-            <a class="nav-link" href="#">Hello</a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#">Courses</a>
+            <a className="nav-link" href="#">Map</a>
+            <a className="nav-link" href="#">Hello</a>
           </div>
         </div>
       </nav>
